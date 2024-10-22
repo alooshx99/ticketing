c43627918b6e6423e3ce4f7cf33e1e5bdd35289e
alooshx99 <allawy2246@gmail.com>
2024-10-22 20:25:16 +0300

    frontend

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminTicketController;
 
 //Route::get('/', function () {
 //    return view('welcome');
 //});
 
 
-Route::get('/', function () {
-    return view('admin');
-});
+//Route::get('/', function () {
+//    return view('dashboard');
+//});
+
+Route::get('/dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'DashboardData']);
+Route::get('/usersList',function (){return view('users');});
+Route::get('/editInfo', [App\Http\Controllers\Admin\DashboardController::class, 'DashboardData']);
+Route::get('/chat', [App\Http\Controllers\Admin\DashboardController::class, 'DashboardData']);
+Route::get('/tickets', [App\Http\Controllers\Admin\DashboardController::class, 'DashboardData']);
 
 
 Route::get('/1', function () {
